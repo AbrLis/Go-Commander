c3ef33b94deda896ce4871fea83aa4a9532178b2
grrrinch <grrrinch@gmail.com>
2020-05-31 14:33:51 +0300

    убрал лишнюю табуляцию

--- commands.go
+++ commands.go
@@ -27,18 +27,15 @@ func LsFunc(path string) error {
 
 	for _, file := range files { //Сначала выводится список папок
 		if file.IsDir() {
-			_, err := dirColor.Printf("/%s\t", file.Name())
+			_, err := dirColor.Printf("/%-10s\t", file.Name())
 			Check(err)
 		}
 	}
 	for _, file := range files { //Затем список файлов
 		if !file.IsDir() {
-			_, err := fileColor.Printf("%s\t", file.Name())
+			_, err := fileColor.Printf("%-10s\t", file.Name())
 			Check(err)
 		}
-		
-		fmt.Print("\t") 
-
 	}
 	fmt.Println() // после вывода списка файлов и папок переводим курсор на новую строку
 
